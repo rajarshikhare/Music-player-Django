b4c20499f883e6d9ae85a179f3e24df30ca437c5
rajarshikhare <rajarshi.vaibhav@gmail.com>
2018-04-21 12:01:36 +0530

    2nd

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.http import HttpResponse
-from .models import Album
+from .models import Album, Songs
 from django.shortcuts import render
 
 def index(request):
@@ -19,4 +19,15 @@ def details(request, album_id):
 	except Album.DoesNotExist:
 		raise Http404("Album not found")
 	return render(request, 'music/test/details.html', {'album': album})
+
+def favorite(request, album_id):
+    album = Album.objects.get(id=album_id)
+    try:
+        selected_song = album.songs_set.get(id=request.POST['song'])
+    except (KeyError, Songs.DoesNotExist):
+        return render(request, 'music/test/details.html', {'album': album})
+    else:
+        selected_song.is_favorite = not selected_song.is_favorite
+        selected_song.save()
+        return render(request, 'music/test/details.html', {'album': album})
 	
